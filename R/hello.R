02e6a67ba98b5dba4144af96646a196af89e3d51
Gustavo Quispe <gustavoaqv@hotmail.com>
2021-12-27 09:06:58 -0500

    For qPCR Relative Quantification Analysis (v1.2)
    Functions description and efficiencies calculation added. Dplyr it's no longer required. Bug Fixes.

--- R/hello.R
+++ R/hello.R
@@ -1,15 +1,24 @@
+#Important info in Realhelp()
+#ALL COMANDS AVAILABLE IN Realtools()
+
 Realhelp <- function(){
   print("for qPCR relative quantification (Pffafl & Livak Method)")
   print("Input csv. dataframe 1 (the qPCR results) columns must have, respectively: target gene Ct, control gene Ct, tissue")
   print("Input csv. dataframe 2 (the standard curve) columns must have, respectively: target gene Ct, control gene Ct, amount of RNA/DNA/cDNA.")
-  print("When using Livak method, dataframe 2 it's not needed")}
+  print("When using Livak method, dataframe 2 it's not needed")
+  print("ALL COMMANDS AVAILABLE IN Realtools()")}
+
+Realtools <- function(){
+  print("For Pfaffl method use qPCR_1(Df1,Df2,Target gene,Control gene,Target tissue, Control tissue)")
+  print("For Livak method use qPCR_2(Df1,Target gene,Control gene,Target tissue, Control tissue)")
+  print("To save Pfaffl results in csv. use Save1()")
+  print("To save Livak results in csv. use Save2()")
+  print("For efficiencies use dataframe 2 and Efi1(Df2) & Efi2(Df2) commands")}
 
 qPCR_1 <- function(a,b,x,y,c,d){
-  install.packages("dplyr")
-  library("dplyr")
   Re <- data.frame(
-    (10^(-1/(unname(round((lm(formula = as.list(b %>% select(1))[[1]]
-                              ~ log10(as.list(b %>% select(3))[[1]])))$coeff[2],digits = 2)))))^
+    (10^(-1/(unname(round((lm(formula = b[[1]]
+                              ~ log10(b[[3]])))$coeff[2],digits = 2)))))^
 
       (c(unname((t.test(a[,x][a[,3] == c]))[["estimate"]])
          ,
@@ -18,8 +27,8 @@ qPCR_1 <- function(a,b,x,y,c,d){
            ,
            t.test(a[,y][a[,3] == c])[["conf.int"]][1:2])),
 
-    (10^(-1/(unname(round((lm(formula = as.list(b %>% select(2))[[1]]
-                              ~ log10(as.list(b %>% select(3))[[1]])))$coeff[2],digits = 2)))))^
+    (10^(-1/(unname(round((lm(formula = b[[2]]
+                              ~ log10(b[[3]])))$coeff[2],digits = 2)))))^
 
       (c(unname((t.test(a[,x][a[,3] == d]))[["estimate"]])
          ,
@@ -29,8 +38,8 @@ qPCR_1 <- function(a,b,x,y,c,d){
            t.test(a[,y][a[,3] == d])[["conf.int"]][1:2])),
 
 
-    (10^(-1/(unname(round((lm(formula = as.list(b %>% select(1))[[1]]
-                              ~ log10(as.list(b %>% select(3))[[1]])))$coeff[2],digits = 2)))))^
+    (10^(-1/(unname(round((lm(formula = b[[1]]
+                              ~ log10(b[[3]])))$coeff[2],digits = 2)))))^
 
       (c(unname((t.test(a[,x][a[,3] == c]))[["estimate"]])
          ,
@@ -38,8 +47,8 @@ qPCR_1 <- function(a,b,x,y,c,d){
          c(unname((t.test(a[,y][a[,3] == c]))[["estimate"]])
            ,
            t.test(a[,y][a[,3] == c])[["conf.int"]][1:2]))/
-      (10^(-1/(unname(round((lm(formula = as.list(b %>% select(2))[[1]]
-                                ~ log10(as.list(b %>% select(3))[[1]])))$coeff[2],digits = 2)))))^
+      (10^(-1/(unname(round((lm(formula = b[[2]]
+                                ~ log10(b[[3]])))$coeff[2],digits = 2)))))^
 
       (c(unname((t.test(a[,x][a[,3] == d]))[["estimate"]])
          ,
@@ -52,12 +61,9 @@ qPCR_1 <- function(a,b,x,y,c,d){
   colnames(Re)<-c("Target_tissue","Control_tissue","Ratio")
   rownames(Re)<-c("Mean","Lower CI","Upper CI")
   qPCR_Results_Pfaffl <<- Re
-  View(qPCR_Results_Pfaffl)
-}
+  View(qPCR_Results_Pfaffl)}
 
 qPCR_2 <- function(a,x,y,c,d){
-  install.packages("dplyr")
-  library("dplyr")
   Re <- data.frame(
     (2)^
 
@@ -86,7 +92,7 @@ qPCR_2 <- function(a,x,y,c,d){
          c(unname((t.test(a[,y][a[,3] == c]))[["estimate"]])
            ,
            t.test(a[,y][a[,3] == c])[["conf.int"]][1:2]))/
-      (2)^
+    (2)^
 
       (c(unname((t.test(a[,x][a[,3] == d]))[["estimate"]])
          ,
@@ -101,6 +107,12 @@ qPCR_2 <- function(a,x,y,c,d){
   qPCR_Results_Livak <<- Re
   View(qPCR_Results_Livak)
 }
+
+Efi1 <- function (b){10^(-1/(unname(round((lm(formula = b[[1]]
+                                              ~ log10(b[[3]])))$coeff[2],digits = 2))))}
+
+Efi2 <- function (b){10^(-1/(unname(round((lm(formula = b[[2]]
+                                              ~ log10(b[[3]])))$coeff[2],digits = 2))))}
 
 Save1 <- function(){write.csv(qPCR_Results_Pfaffl,"qPCR_Results_Pfaffl.csv",row.names = FALSE)
 }
